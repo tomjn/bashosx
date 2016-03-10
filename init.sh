4e51c3df382d68429c0006f81166f0f94c9458ba
Tom J Nowell <contact@tomjn.com>
2016-03-10 05:16:31 +0000

    improve the initial ssh response to contain colours, username, and server name

--- init.sh
+++ init.sh
@@ -35,5 +35,5 @@ fi
 
 # if we're a remote server, print out some info when opening a new session such as current load
 if [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ]; then
-        uptime
+        printf "\nHello \e[1;33m$(whoami)\e[0m, you are connected to \e[1;33m$(hostname)\e[0m, current uptime is:\n\n\e[1;32m$(uptime)\n\n"
 fi
