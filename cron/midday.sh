78380f6ad9cfd7dcf8bbb0275b27407508972efc
Tom J Nowell <contact@tomjn.com>
2016-06-17 12:17:09 +0100

    Homebrew autoupdating that works

--- cron/midday.sh
+++ cron/midday.sh
@@ -1,8 +1,13 @@
-# stop procrastinating by running brew update, automate the thing away
-/usr/local/bin/brew update
-/usr/local/bin/brew upgrade
-/usr/local/bin/brew cleanup
-/usr/local/bin/brew prune
+#!/bin/bash
 
+brew=/usr/local/bin/brew
+logger=/usr/bin/logger
+
+$brew update 2>&1  | $logger -t brewup.update
+$brew upgrade 2>&1 | $logger -t brewup.upgrade
+$brew cleanup 2>&1 | $logger -t brewup.cleanup
+$brew prune 2>&1   | $logger -t brewup.prune
+
+# not needed as homebrew will take care of this, but uncomment if necessary
 # also keep composer in good shape
-/usr/local/bin/composer selfupdate
+#/usr/local/bin/composer selfupdate
