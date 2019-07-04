50d65c769c3fadf17fd413e059fd111509a2868c
Tom J Nowell <contact@tomjn.com>
2019-07-04 17:11:00 +0100

    handle ruby and .local path

--- paths.sh
+++ paths.sh
@@ -1,6 +1,8 @@
 export PATH=/usr/local/share/npm/bin:$PATH
 export PATH=~/.composer/vendor/bin:$PATH
 export PATH="/usr/local/sbin:$PATH"
+export PATH=~/.local/bin:$PATH
+export PATH=/usr/local/lib/ruby/gems/2.6.0/bin:$PATH
 
 # if homebrew is available, set the PATH
 if hash brew 2>/dev/null; then
